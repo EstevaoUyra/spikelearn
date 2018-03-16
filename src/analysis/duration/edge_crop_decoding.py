6f406ed4faa902bce066027ae60a676c3cc1c8c6
Estevão Uyrá Pardillos Vieira <estevao.uyra.pv@gmail.com>
2018-03-16 08:36:04 -0300

    Edge crop decoding

---
+++ src/analysis/duration/edge_crop_decoding.py
@@ -0,0 +1,49 @@
+"""Takes off the edges and predicts, until there is nothing left."""
+
+from itertools import product, cycle, chain
+import pandas as pd
+import numpy as np
+import sys
+sys.path.append('.')
+
+from spikelearn.measures.similarity import unit_similarity_evolution
+from spikelearn.models.shuffle_decoding import shuffle_val_predict
+from spikelearn.data import io, to_feature_array, select, SHORTCUTS, remove_baseline
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+
+import os
+import pickle
+
+# Parameters
+TMIN = 1.5;
+n_trial_per_splits = 100
+folder = 'data/results/across_trials/edge_crop_decoding/'
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+DSETS = ['narrow_smoothed', 'narrow_smoothed_norm']#['medium_smoothed', 'medium_smoothed_norm',
+        # 'narrow_smoothed', 'narrow_smoothed_norm',
+        # 'wide_smoothed']
+NSPLITS = 100
+subset = 'cropped'
+
+clf = LogisticRegression()
+for label, dset in product( SHORTCUTS['groups']['DRRD'], DSETS ):
+
+    data = select( io.load(label, dset), _min_duration=1.5,
+                    is_selected=True, is_tired=False )
+    data = to_feature_array(select)
+
+    times = df.reset_index().time.unique()
+
+    res = []
+    for crop in range(len(times - 1)//2):
+        to_use_times = times[crop:-crop]
+        df = select(data.reset_index(),
+                time_in_=to_use_times).set_index(['trial', 'time'])
+        res.append(shuffle_val_predict(clf, df, n_splits = NSPLITS) )
+
+    pickle.dump(res, open('{}{}_{}'.format(folder, label, dset),'wb') )
